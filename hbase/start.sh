bd1c0a8fdfeaef2b6d8311ed67687ee72858d70e
Karl Stoney <kstoney@thoughtworks.com>
2017-02-10 23:13:23 +0000

    Got HBase and Hue working in Distributed mode!

--- hbase/start.sh
+++ hbase/start.sh
@@ -3,6 +3,17 @@ set -e
 HOSTNAME=$(hostname)
 QUORUM=${QUORUM:-$HOSTNAME}
 
+if [ "$USE_HDFS" = "true" ]; then
+  cp /opt/hbase/conf/hbase-site.hdfs.xml /opt/hbase/conf/hbase-site.xml
+else
+  rm /etc/supervisord.d/zookeeper.service.conf
+  rm /etc/supervisord.d/regionserver.service.conf
+  cp /opt/hbase/conf/hbase-site.local.xml /opt/hbase/conf/hbase-site.xml
+fi
+
+rm /opt/hbase/conf/hbase-site.local.xml
+rm /opt/hbase/conf/hbase-site.hdfs.xml
+
 sed -i "s/HOSTNAME/$HOSTNAME/g" /opt/hbase/conf/hbase-site.xml
 sed -i "s/QUORUM/$QUORUM/g" /opt/hbase/conf/hbase-site.xml
 
