26fcfa09991943f5057468d82f2d738acb706230
Karl Stoney <kstoney@thoughtworks.com>
2017-02-12 19:31:12 +0000

    Fixed lily add indicies

--- lily/add_indices.sh
+++ lily/add_indices.sh
@@ -7,13 +7,13 @@ do
 	WITHOUT=${varname/INDEX_COLLECTION_/}
 
 	INDEX=${WITHOUT}_idx
-    FILE=/data/indices/${WITHOUT}.xml
+	FILE=/data/indices/${WITHOUT}.xml
 	COLLECTION=${!varname}
-	
+
 	echo "  -> Creating $INDEX, from $FILE, populating collection $COLLECTION"
-    ./bin/hbase-indexer add-indexer -n $INDEX \
-	-c $FILE \
-	-cp solr.zk=$HBASE_CONF_QUORUM/solr \
-	-cp solr.collection=$COLLECTION \
-	--zookeeper $HBASE_CONF_QUORUM:2181 
+	./bin/hbase-indexer add-indexer -n $INDEX \
+		-c $FILE \
+		-cp solr.zk=$HBASE_CONF_QUORUM \
+		-cp solr.collection=$COLLECTION \
+		--zookeeper $HBASE_CONF_QUORUM:2181 
 done
