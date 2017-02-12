0997c8b67aeb0d1a27ed1794c44a98a56b26f4f7
Karl Stoney <kstoney@thoughtworks.com>
2017-02-12 18:47:34 +0000

    Fixed a config issue

--- demo.sh
+++ demo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -e 
+set -e
+
+if ! hash jq 2>/dev/null; then
+  echo You need JQ installed matey!  Please install it first.
+  exit 1
+fi
+ 
 echo Starting HDFS...
 docker-compose up -d namenode datanode1 datanode2 
 
@@ -18,18 +24,28 @@ docker-compose up -d hue
 echo Creating Users table in HBase...
 docker-compose exec hbase_master /bin/sh -c "echo \"create 'User', { NAME => 'cf', REPLICATION_SCOPE => '1' }\" | hbase shell"
 
-echo Creating Templates in Hue...
-API="http://127.0.0.1:8082/nifi-api"
-PROCESS_GROUP=$(curl --silent $API/flow/process-groups/root | jq .processGroupFlow.id)
-PROCESS_GROUP=$(echo $PROCESS_GROUP | tr -d '"')
+export WEB="http://127.0.0.1:8082/nifi"
+export API="http://127.0.0.1:8082/nifi-api"
+echo Waiting for NiFi to come up...
+until $(curl --output /dev/null --silent --head --fail $WEB); do
+  printf '.'
+  sleep 2
+done
 
-curl -iv -F template=@config/hue/users.xml $API/process-groups/$PROCESS_GROUP/templates/upload  
+echo Creating Templates in NiFi...
+export PROCESS_GROUP=$(curl --silent $API/flow/process-groups/root | jq .processGroupFlow.id)
+export PROCESS_GROUP=$(echo $PROCESS_GROUP | tr -d '"')
+echo Process group: $PROCESS_GROUP
 
-TEMPLATE_ID=$(curl --silent $API/flow/templates | jq .templates | jq '.[]'.id)
-TEMPLATE_ID=$(echo $TEMPLATE_ID | tr -d '"')
+curl -iv -F template=@config/nifi/users.xml $API/process-groups/$PROCESS_GROUP/templates/upload > /dev/null
+ 
+export TEMPLATE_ID=$(curl --silent $API/flow/templates | jq .templates | jq '.[]'.id)
+export TEMPLATE_ID=$(echo $TEMPLATE_ID | tr -d '"')
+echo Template id: $TEMPLATE_ID
 
-PAYLOAD="{\"templateId\":\"$TEMPLATE_ID\",\"originX\":365,\"originY\":-21}"
+export PAYLOAD="{\"templateId\":\"$TEMPLATE_ID\",\"originX\":365,\"originY\":-21}"
 
-curl -kH "Content-Type: application/json" -X POST -d $PAYLOAD $API/process-groups/$PROCESS_GROUP/template-instance
+curl -kH "Content-Type: application/json" -X POST -d $PAYLOAD $API/process-groups/$PROCESS_GROUP/template-instance > /dev/null
 
-open "http://127.0.0.1:8082/nifi"
+echo Opening NiFi...
+open $WEB 
