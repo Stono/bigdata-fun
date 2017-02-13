ec59c605b259951586f99f1db1d1f16e325a18d6
Karl Stoney <kstoney@thoughtworks.com>
2017-02-13 11:16:49 +0000

    Ensured solr creates zookeeper node /solr

--- solr/run.sh
+++ solr/run.sh
@@ -10,4 +10,14 @@ if [ ! -f "/data/solr.xml" ]; then
   cp -R /opt/solr/server/solr/configsets /data/
 fi
 
-su -c './bin/solr -s /data -p 8983 -f -c -z hbase-zookeeper:2181' solr
+echo Checking solr zookeeper node exists on $HBASE_CONF_QUORUM...
+set +e
+su -c "./bin/solr zk ls /solr -z $HBASE_CONF_QUORUM:2181 &>/dev/null" &>/dev/null solr
+EXIT_CODE=$?
+set -e
+
+if [ ! "$EXIT_CODE" = "0" ]; then
+  echo Creating solr zookeeper node...
+  su -c "./bin/solr zk mkroot /solr -z $HBASE_CONF_QUORUM:2181"
+fi
+su -c "./bin/solr -s /data -p 8983 -f -c -z $HBASE_CONF_QUORUM:2181/solr" solr
