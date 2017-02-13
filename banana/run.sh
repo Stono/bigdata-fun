c8d598a2069541ae53b3e7e89ddc12b4830f7cf9
Karl Stoney <kstoney@thoughtworks.com>
2017-02-13 20:39:47 +0000

    Updated default solr collections for lil and banana

--- banana/run.sh
+++ banana/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+# Configure banana
 sed -i -e "s,solr: \"/.*/\",solr: \"$SOLR_HOST:$SOLR_PORT/$SOLR_DOMAIN\",g" /opt/banana/src/config.js
+
+# Configure the default dashboard
 sed -i -e "s,\"server\": \"/.*/\",\"server\": \"$CLIENT_SOLR_HOST:$CLIENT_SOLR_PORT/$SOLR_DOMAIN\",g" /opt/banana/src/app/dashboards/default.json
+sed -i -e "s,collection1,$CLIENT_INITIAL_COLLECTION,g" /opt/banana/src/app/dashboards/default.json
 
 cd /opt/banana
 ./node_modules/http-server/bin/http-server -a 0.0.0.0 -p 8000
