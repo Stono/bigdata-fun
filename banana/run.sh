aadc13d916ac6c7a68a58b2e935d1971871d2f9e
Karl Stoney <kstoney@thoughtworks.com>
2017-02-13 20:31:48 +0000

    fixed cross-domain issue with Banana and Solr

--- banana/run.sh
+++ banana/run.sh
@@ -2,7 +2,7 @@
 set -e
 
 sed -i -e "s,solr: \"/.*/\",solr: \"$SOLR_HOST:$SOLR_PORT/$SOLR_DOMAIN\",g" /opt/banana/src/config.js
-sed -i -e "s,\"server\": \"/.*/\",\"server\": \"$SOLR_HOST:$SOLR_PORT/$SOLR_DOMAIN\",g" /opt/banana/src/app/dashboards/default.json
+sed -i -e "s,\"server\": \"/.*/\",\"server\": \"$CLIENT_SOLR_HOST:$CLIENT_SOLR_PORT/$SOLR_DOMAIN\",g" /opt/banana/src/app/dashboards/default.json
 
 cd /opt/banana
 ./node_modules/http-server/bin/http-server -a 0.0.0.0 -p 8000
